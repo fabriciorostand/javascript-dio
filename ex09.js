a91e7dae787ab3d836488e5bd711a0d513e30c1d
fabriciorostand <fabriciorostand2000@gmail.com>
2024-12-06 19:31:37 -0300

    Update ex09.js

--- ex09.js
+++ ex09.js
@@ -19,4 +19,18 @@ class Pessoa {
 const fabricio = new Pessoa('Fabricio Rostand', 'Brasília', 23);
 const elena = new Pessoa('Elena Cordeiro', 'Brasília', 24);
 
+// Funções recebendo Objetos
+
+function compararPessoas (p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(p1.nome + 'é mais velho(a) que' + p2.nome);
+    } else if (p2.idade > p1.idade) {
+        console.log(p2.nome + ' é mais velho(a) que ' + p1.nome)
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`)
+    }
+
+}
+
 fabricio.descrever();
+compararPessoas(fabricio, elena);
