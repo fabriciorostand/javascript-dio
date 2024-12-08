2a91854e0c12c1b1b5bcc961fd68bca345db6a22
fabriciorostand <fabriciorostand2000@gmail.com>
2024-12-08 01:15:31 -0300

    Create ex012.js

---
+++ ex012.js
@@ -0,0 +1,24 @@
+// Arrays
+
+const pessoas = ['Fabricio', 'Elena', 'Jaqueline'];
+console.log(pessoas);
+
+// Adicionando valores em um Array  
+
+pessoas.push('Isabelli');
+console.log(pessoas);
+
+// Outra forma de adicionar valores em Arrays
+
+pessoas[4] = 'Giuseppe Camolli';
+console.log(pessoas);
+
+// Removendo o último item de Arrays
+
+pessoas.pop();
+console.log(pessoas);
+
+// Removendo o primeiro item de Arrays
+
+pessoas.shift();
+console.log(pessoas);
